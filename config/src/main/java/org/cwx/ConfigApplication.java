caa55e3c70760cf516c386ff250cd2332c06bc84
weicanx <weic1228@gmail.com>
2018-06-08 15:11:48 +0800

    add config repo

--- config/src/main/java/org/cwx/ConfigApplication.java
+++ config/src/main/java/org/cwx/ConfigApplication.java
@@ -1,18 +1,18 @@
 package org.cwx;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by weican on 2018/6/8.
  */
-@Configuration
-@EnableAutoConfiguration
+//@Configuration
+//@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigApplication {
 
     public static void main(String[] args) {
